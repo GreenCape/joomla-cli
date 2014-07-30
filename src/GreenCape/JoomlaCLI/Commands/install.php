ed88cefff355e71dc2ca3033c3ccc4512a452eea
Niels Braczek <nbraczek@bsds.de>
2014-07-30 11:34:54 +0200

    Use Symfony/Command as BaseCommand

--- src/GreenCape/JoomlaCLI/Commands/install.php
+++ src/GreenCape/JoomlaCLI/Commands/install.php
@@ -45,7 +45,7 @@
 
 namespace GreenCape\JoomlaCLI;
 
-use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\Command as BaseCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
